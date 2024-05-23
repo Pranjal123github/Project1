3120ef273430f3405eda4f62c1be4f30eed9dfbc
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-23 13:29:47 +0530

    My First Commit

---
+++ src/com/pranjali/example/Example.java
@@ -0,0 +1,23 @@
+package com.pranjali.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Example ex = new Example();
+		List<String> list = ex.listOfTopic();
+		System.out.println(list);
+	}
+
+	public List<String> listOfTopic() {
+		List<String> list = new ArrayList<>();
+		list.add("Core Java");
+		list.add("Collection Freamework");
+		list.add("SQL");
+		list.add("Git");
+		return list;
+	}
+
+}
