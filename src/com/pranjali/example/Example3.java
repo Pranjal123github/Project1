d01002f6f3f6c6d5c8ade4d7870cd1c9d57fac5d
Pranjal123github <pranjalisatpute0891@gmail.com>
2024-05-27 13:10:18 +0530

    Added Conflict Message

--- src/com/pranjali/example/Example3.java
+++ src/com/pranjali/example/Example3.java
@@ -5,6 +5,8 @@ public class Example3 {
 
 		System.out.println("Welcome To Git...");
 		System.out.println('p'+'r'+'a'+'n'+'j'+'a'+'l'+'i');
+		System.out.println("Welcome To Conflict Resolution...");
+
 	}
 	
 }
