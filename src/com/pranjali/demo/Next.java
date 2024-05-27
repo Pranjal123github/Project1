944372e22e534470885cd0949f09b43fe4f5daeb
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-27 17:16:51 +0530

    Added

--- src/com/pranjali/demo/Next.java
+++ src/com/pranjali/demo/Next.java
@@ -4,6 +4,7 @@ public class Next {
 public static void main(String[] args) {
 	System.out.println("From Next Class");
 	System.out.println("Pranjali");
-	System.out.println(" Next Class");
+	System.out.println("Dev 2-Changes");
+	System.out.println("Bhumika");
 }
 }
