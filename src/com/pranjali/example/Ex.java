54e40b0dc4cc1a44bdf113c950800d0be1523f6a
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-27 14:53:42 +0530

    One Added

--- src/com/pranjali/example/Ex.java
+++
@@ -1,9 +0,0 @@
-package com.pranjali.example;
-
-public class Ex {
-public static void main(String[] args) {
-	System.out.println("hii");
-	System.out.println("Hello");
-	System.out.println("Namskar");
-}
-}
