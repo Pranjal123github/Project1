e85f32e035551dcced1639ad996019225f343081
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-27 15:01:21 +0530

    Added New Code

--- src/com/pranjali/example/One.java
+++ src/com/pranjali/example/One.java
@@ -3,7 +3,7 @@ package com.pranjali.example;
 public class One {
 	public static void main(String[] args) {
 		System.out.println("Negative");
-		System.out.println("Zero");
+		System.out.println("Nine");
 		System.out.println("One");
 	}
 }
