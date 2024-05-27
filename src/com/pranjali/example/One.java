ceec03fa019557a0237c7ccb3b213c72ebfcfc91
Pranjal123github <pranjalisatpute0891@gmail.com>
2024-05-27 15:00:15 +0530

    Added Conflict Message

--- src/com/pranjali/example/One.java
+++ src/com/pranjali/example/One.java
@@ -3,7 +3,7 @@ package com.pranjali.example;
 public class One {
 	public static void main(String[] args) {
 		System.out.println("Negative");
-		System.out.println("Zero");
-		System.out.println("One");
+		System.out.println("Six");
+		System.out.println("Seven");
 	}
 }
