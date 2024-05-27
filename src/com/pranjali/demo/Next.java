4dadb10eec7c53359cba2d261960432e931c2a53
Pranjal123github <pranjalisatpute0891@gmail.com>
2024-05-27 17:13:45 +0530

    Update Added

--- src/com/pranjali/demo/Next.java
+++ src/com/pranjali/demo/Next.java
@@ -4,6 +4,6 @@ public class Next {
 public static void main(String[] args) {
 	System.out.println("From Next Class");
 	System.out.println("Pranjali");
-	System.out.println(" Next Class");
+	System.out.println(" Dev 1-Next Class");
 }
 }
