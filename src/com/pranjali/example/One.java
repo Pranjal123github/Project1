961b97aa40a4366ea202009bd7ea00d9fc207590
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-27 14:57:22 +0530

    Added Negative

--- src/com/pranjali/example/One.java
+++ src/com/pranjali/example/One.java
@@ -1,8 +1,9 @@
 package com.pranjali.example;
 
 public class One {
-public static void main(String[] args) {
-	System.out.println("Zero");
-	System.out.println("One");
-}
+	public static void main(String[] args) {
+		System.out.println("Negative");
+		System.out.println("Zero");
+		System.out.println("One");
+	}
 }
