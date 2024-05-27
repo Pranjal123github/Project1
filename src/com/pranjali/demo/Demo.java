6704f1c25ad9dab4d1d2d264ab2e50fbaae06158
Pranjali Satpute <pranjalisatpute0891@gmail.com>
2024-05-27 17:04:04 +0530

    Added

--- src/com/pranjali/demo/Demo.java
+++ src/com/pranjali/demo/Demo.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
 	
 	System.out.println("From Demo");
 	System.out.println("Dev-1 Master Changes");
+	System.out.println("Learning Java");
+	System.out.println("Learning Design Patternes");
 }
 }
